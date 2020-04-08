21423f6a9a03b7d4536ed3caa2aac5b2e7c5e7f7
james w hubert <jwhubert91@gmail.com>
2020-04-08 17:07:24 -0400

    Initial commit

---
+++ src/components/TodoListItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class TodoListItem extends Component {
+	render() {
+		return (
+			<div>
+				
+			</div>
+		)
+	}
+}
+
+export default TodoListItem
