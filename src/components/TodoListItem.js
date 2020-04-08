e313c9951665cfe7bbeaee9e1f63144fb1e1a1a2
james w hubert <jwhubert91@gmail.com>
2020-04-08 18:12:09 -0400

    Add PropTypes to component files

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
 
 export class TodoListItem extends Component {
 	render() {
 		return (
 			<div>
-				
+				<p>{ this.props.todo.title }</p>
 			</div>
 		)
 	}
+}
+
+// PropTypes
+TodoListItem.propTypes = {
+	todo: PropTypes.object.isRequired
 }
 
 export default TodoListItem
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import TodoListItem from './TodoListItem';
+import PropTypes from 'prop-types';
 
 class Todos extends Component {
 	render() {
 		return this.props.todos.map((todo) => (
-			<h3>{ todo.title }</h3>
+			<TodoListItem key={todo.id} todo={todo}/>
 		));
 	}
+}
+
+// PropTypes
+Todos.propTypes = {
+	todos: PropTypes.array.isRequired
 }
 
 export default Todos;
